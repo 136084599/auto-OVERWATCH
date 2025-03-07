d216bc2988df01c75335c1f29a0caf31af2aa963
136084599 <90099110+136084599@users.noreply.github.com>
2025-03-07 14:18:41 +0800

    Add files via upload

---
+++ ow.py
@@ -0,0 +1,39 @@
+
+import pyautogui
+import time
+
+while True:
+        # 模拟鼠标点击
+        time.sleep(1)
+        pyautogui.click(x=2467, y=465)
+        pyautogui.click(x=2467, y=465)
+        pyautogui.click(x=2391, y=477)
+      #  time.sleep(1)
+      #  pyautogui.click(x=592, y=491)
+      #  pyautogui.click(x=592, y=491)
+        time.sleep(20)
+        pyautogui.press('e')
+        time.sleep(3)
+        pyautogui.press('e')
+        time.sleep(5)
+        pyautogui.press('e')
+        pyautogui.press('esc')
+        time.sleep(1)
+        pyautogui.click(x=2945, y=631)
+        pyautogui.click(x=2945, y=631)
+        time.sleep(1)
+        pyautogui.click(x=2957, y=587)
+        pyautogui.click(x=2957, y=587)
+       # pyautogui.press('space')
+        time.sleep(3)
+        pyautogui.click(x=2885, y=951)
+        # pyautogui.press('space')
+        time.sleep(1)
+        pyautogui.click(x=2885, y=951)
+        # pyautogui.press('space')
+        time.sleep(1)
+        pyautogui.click(x=2885, y=951)
+        #pyautogui.press('space')
+        #pyautogui.press('space')
+
+
